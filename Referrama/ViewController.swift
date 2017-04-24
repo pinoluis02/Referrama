438d8f54c7d95fb6c855fa9dddc9618be80cc3bd
Luis Rufino Perez Romero <LuisPerez@MacBook-Air-de-Luis.local>
2017-04-23 20:25:32 -0400

    Initial Commit

---
+++ Referrama/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Referrama
+//
+//  Created by Luis Rufino Perez Romero on 4/23/17.
+//  Copyright © 2017 LuisPerez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
